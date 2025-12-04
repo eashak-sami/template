1ce89e9b9f407c556612f174e8db42759281b124
Md Eashaque Rahman Sami <sami.eashaque@konasl.com>
2025-12-04 19:33:09 +0600

    code refactor

--- src/main/java/com/ksl/template/application/repository/SampleRepository.java
+++ src/main/java/com/ksl/template/application/repository/SampleRepository.java
@@ -14,20 +14,20 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface SampleRepository extends JpaRepository<SampleEntity, Long> {
 
-  List<SampleEntity> findAllByStatus(Status status);
+    List<SampleEntity> findAllByStatus(Status status);
 
-  @Query("""
-      SELECT new com.ksl.template.api.payload.response.SampleSearchResponse(
-          s.id, s.reqVar1, s.reqVar2, s.notNullableVar, s.uniqueVar, s.status
-      )
-      FROM SampleEntity s
-      WHERE (:reqVar1 IS NULL OR s.reqVar1 = :reqVar1)
-        AND (:reqVar2 IS NULL OR s.reqVar2 = :reqVar2)
-        AND (:status IS NULL OR s.status = :status)
-      """)
-  Page<SampleSearchResponse> search(
-      @Param("reqVar1") String reqVar1,
-      @Param("reqVar2") String reqVar2,
-      @Param("status") Status status,
-      Pageable pageable);
+    @Query("""
+            SELECT new com.ksl.template.api.payload.response.SampleSearchResponse(
+                s.id, s.reqVar1, s.reqVar2, s.notNullableVar, s.uniqueVar, s.status
+            )
+            FROM SampleEntity s
+            WHERE (:reqVar1 IS NULL OR s.reqVar1 = :reqVar1)
+              AND (:reqVar2 IS NULL OR s.reqVar2 = :reqVar2)
+              AND (:status IS NULL OR s.status = :status)
+            """)
+    Page<SampleSearchResponse> search(
+            @Param("reqVar1") String reqVar1,
+            @Param("reqVar2") String reqVar2,
+            @Param("status") Status status,
+            Pageable pageable);
 }
